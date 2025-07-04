917feaa7c3da3eb76538830c363ff70814d2b8bd
suhana0709 <farjanajaman1975@gmail.com>
2025-07-04 08:14:34 -0500

    Add files via upload

---
+++ computer price.py
@@ -0,0 +1,22 @@
+#creating class
+class Computer():
+
+    def __init__(self):
+        self.__max_price = 900
+
+    def sell():
+        print("Selling Price: {}".format(self.__max_price))
+
+    def setMaxPrice(self, price):
+        self.__max_price = price
+
+#creating object
+c = Computer()
+
+#changing the value
+c.__max-price = 1000
+c.sell()
+
+#using the setter function
+c.setMaxPrice = 1000
+c.sell()
